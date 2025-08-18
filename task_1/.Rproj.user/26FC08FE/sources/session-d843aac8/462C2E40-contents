f1848e601c65ef94052f3e3a1a8e77f1b8007189
yuknakas <yukinakashiba@gmail.com>
2025-08-19 00:04:11 +0900

    initial commit'

---
+++ task_1/.Rproj.user/26FC08FE/sources/session-d843aac8/462C2E40-contents
@@ -0,0 +1,36 @@
+# loading libraries
+library("tidyverse")
+library("readxl")
+library("labelled")
+
+# defining paths
+path_raw <- "data/raw/学級数"
+path_out <- "data/original"
+
+# making a df list
+df_list <- 
+  list.files(path_raw, full.names = TRUE) %>% 
+  map(read_excel) %>% 
+  map(
+    \(df){
+      n_class <- ncol(df) - 2
+      df <- set_names(
+        df,
+        c("prefecture", "total", paste0(0:(n_class - 1), "_class"))
+        )
+      base_label <- 
+        list(
+          prefecture = "Name of Prefecture",
+          total = "Total Numbr of Schools in Prefecture"
+        )
+      class_label <- 
+        setNames(
+          paste0("NO. of Schools with ", 0:(n_class - 1), " class(es)"),
+          paste0(0:(n_class - 1), "_class")
+        )
+      var_label(df) <- c(base_label, class_label)
+      df
+      }
+  )
+
+saveRDS(df_list, file = file.path(path_out, "10y_list"))
---
+++ task_1/code/clean_absence.R
@@ -0,0 +1,37 @@
+# Loading one package into the library
+library("tidyverse")
+
+# defining paths for easier access later on
+path_absent_rds <- "data/original/absent_list.rds"
+path_total_rds <- "data/original/total_data.rds"
+target_result <- "data/cleaned/cleaned.rds"
+
+# reading the rds files
+df_absent_list <- readRDS(path_absent_rds)
+df_total <- readRDS(path_total_rds)
+
+# removing the blank lines
+df_absent_list <- df_absent_list %>% 
+  map(select, -blank) # remove the blank column
+
+# combining the data and correcting the data type
+df_absent <- bind_rows(df_absent_list, .id = "year") %>% 
+  mutate(
+    n_absent = as.numeric(n_absent),
+    year = as.numeric(year),
+  )
+
+# correcting the data type
+df_total <- df_total %>% 
+  mutate(
+    n_total = as.numeric(n_total),
+    year = as.numeric(year),
+  )
+
+#connecting the df, then reordring the header
+df <- df_absent %>% 
+  left_join(df_total, by = c("year", "prefecture")) %>% 
+  select("prefecture", "year", "n_total", "n_absent") %>% 
+  mutate(fraction = n_absent / n_total)
+
+saveRDS(df, file = target_result)
